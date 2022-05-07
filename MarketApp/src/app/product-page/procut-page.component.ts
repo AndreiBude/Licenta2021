4a2656b60db4778d6168e13e333a4f62debde523
AndreiBude <budeandrei07@gmail.com>
2022-05-07 13:38:21 +0300

    Expended photo functionality

--- MarketApp/src/app/product-page/procut-page.component.ts
+++ MarketApp/src/app/product-page/procut-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataTestService, ListingService, ReviewService } from '../shared/data-test.service';
+import { AccountService, DataTestService, ListingService, ReviewService } from '../shared/data-test.service';
 import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-procut-page',
@@ -7,20 +7,26 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./procut-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
-
+  
   constructor(public service:ListingService,
     public _route:ActivatedRoute,
     public username:DataTestService,
     private _router:Router,
-    public review:ReviewService
+    public review:ReviewService,
+    public accountService:AccountService
     ) { }
-
+    sameUser:boolean;
   ngOnInit(): void {
     const id = +this._route.snapshot.params['id'];
     this.service.getListing(id);
     const userID = +this._route.snapshot.params['userid'];
     this.username.getUserById(userID);
     this.review.getReviewsById(userID);
+    this.accountService.currentUser$.subscribe(user => {
+      this.sameUser=(user.userID==userID);
+    },error =>{
+      console.log(error);
+    })
   }
   getUser(){
     this.username.getUserById(this.service.listing.userID);
